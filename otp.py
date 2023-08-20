78c9fe14bfb98e2fa3dd54eb78e0d16b43e5c865
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2023-08-20 21:55:34 +0530

    Delete otp.py

--- otp.py
+++
@@ -1,9 +0,0 @@
-import random
-
-otp = random.randrange(100000, 1000000)
-print(otp)
-user=int(input("Enter OTP:: "))
-if user== otp:
-    print("Access granted!!!!")
-else:
-    print("Access Denied!!!!")
