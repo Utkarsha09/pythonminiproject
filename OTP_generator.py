e5ba5fc15a798a2718e1dc0f7629916d38b0faf5
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2023-08-20 21:55:14 +0530

    Update and rename OTP_generator to OTP_generator.py

---
+++ OTP_generator.py
@@ -0,0 +1,10 @@
+
+import random
+
+otp = random.randrange(100000, 1000000)
+print(otp)
+user=int(input("Enter OTP:: "))
+if user== otp:
+    print("Access granted!!!!")
+else:
+    print("Access Denied!!!!")
